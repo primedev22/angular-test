17681e46be073321ed87a978672fb851dc703927
Prime Dev <prime.dev22@gmail.com>
2020-09-28 10:11:58 +0900

    - Integrate NgBootstrap - Add CookieBannerComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { CookieBannerComponent } from './cookie-banner/cookie-banner.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-test';
+export class AppComponent implements OnInit {
+  constructor(private modalService: NgbModal) {
+  }
+
+  ngOnInit(): void {
+    this.modalService.open(CookieBannerComponent);
+  }
 }
